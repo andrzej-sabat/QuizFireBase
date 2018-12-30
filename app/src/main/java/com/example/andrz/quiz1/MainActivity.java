e288eac6e9be750e1d5249a162115ba1a2f33f40
andrzej-sabat <37102669+andrzej-sabat@users.noreply.github.com>
2018-12-30 22:43:18 +0100

    3

--- app/src/main/java/com/example/andrz/quiz1/MainActivity.java
+++ app/src/main/java/com/example/andrz/quiz1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.andrz.quiz1;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
@@ -10,22 +11,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.example.andrz.quiz1.Common.Common;
 import com.example.andrz.quiz1.Model.User;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
+import android.app.ProgressDialog;
+import android.util.Log;
 
 
 
 public class MainActivity extends AppCompatActivity {
     MaterialEditText edtNewUser,edtNewPassword,edtNewEmail;
-    MaterialEditText edtUser, edtPassword;
+    MaterialEditText edtUser;
+    MaterialEditText edtPassword;
+    String edtEmail;
 
     Button btnSignUp , btnSignIn;
 
@@ -43,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         edtUser = findViewById(R.id.edtUserName);
         edtPassword = findViewById(R.id.edtPassword);
+
 
         btnSignIn = findViewById(R.id.btn2);
         btnSignUp = findViewById(R.id.btn1);
@@ -87,6 +91,9 @@ public class MainActivity extends AppCompatActivity {
                         
                         else 
                             Toast.makeText(MainActivity.this, "Złe hasło", Toast.LENGTH_SHORT).show();
+
+                            edtEmail = login.getEmail();
+                            sendMessage(edtEmail);
 
                         
                     }
@@ -157,6 +164,28 @@ public class MainActivity extends AppCompatActivity {
 
         });
        alertDialog.show();
+    }
+    private void sendMessage(final String email) {
+        final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+        dialog.setTitle("Wysyłanie wiadomości");
+        dialog.setMessage("Czekaj");
+        dialog.show();
+        Thread sender = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    GMailSender sender = new GMailSender("quizapp1337@gmail.com", "856z2z7p");
+                    sender.sendMail("QuizApp",
+                            "Administrator informuje o nieudanej próbie logowania do twojego konta w naszej grze",
+                            "quizapp1337@gmail.com",
+                            email);
+                    dialog.dismiss();
+                } catch (Exception e) {
+                    Log.e("mylog", "Error: " + e.getMessage());
+                }
+            }
+        });
+        sender.start();
     }
 }
 
